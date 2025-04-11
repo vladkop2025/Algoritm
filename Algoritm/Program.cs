2bda8e53e2ff1ecf621529c0d5345ff2914b2560
vladkop2025 <vladkop@mail.ru>
2025-04-11 18:03:14 +0300

    unit1

--- Algoritm/Program.cs
+++ Algoritm/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using BenchmarkDotNet.Running;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Testing>();
+    }
+}
---
+++ Algoritm/Testing.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+public class Testing
+{
+    [Benchmark]
+    public void CreateMatrix()
+    {
+        int n = 10000;
+
+        var matrix = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = new int[n];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] = i + j;
+            }
+        }
+    }
+}
